c48bf44b9a9cd555be1894dff412ca3edde4158b
kilker12 <joey@mjhosting.net>
2013-05-16 10:00:58 -0400

    Update UserData.java

--- src/com/craftrealms/CREssentials/UserData.java
+++ src/com/craftrealms/CREssentials/UserData.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
-public class UserData {
+public class UserData extends Player {
 	private FileConfiguration file;
 	private String p;
 	
